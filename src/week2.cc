4efd59fd751a9f7b1b215a3b35936d5f58b73dd4
Robin A. Dorstijn <robbiedorstijn@gmail.com>
2020-11-17 14:04:02 +0100

    Remove unecessesary executable

--- src/week2.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "week2.h"
-
-int main() {
-    Complex c = Complex(1, 1);
-    Complex c2 = Complex(1, 1);
-    bool val = c == c2;
-    std::cout << val << std::endl;
-}
